e8de95f71855ffc5f42cd60fbdcc1379068c2aa4
DrRhea <czajr13@gmail.com>
2024-04-13 14:32:15 +0700

    minor update for contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,10 @@ import "./contact.css"
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className="contact section" id="contact">
+      <h2 className="section__title">Get in touch</h2>
+      <span className="section__subtitle">Contact Me</span>
+    </section>
   )
 }
 
