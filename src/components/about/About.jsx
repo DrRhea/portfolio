a74c7a4afe10c573383c1d3cb3fc16bd1dbcd4cb
DrRhea <czajr13@gmail.com>
2024-04-13 19:46:39 +0700

    update resume

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./about.css"
 import AboutImg from "../../assets/about.jpeg"
-import CV from "../../assets/blank.pdf"
+import CV from "../../assets/Arya Jagadditha - Full-Stack Developer - Resume.pdf"
 import Info from "./Info"
 
 const About = () => {
