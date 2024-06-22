3624f22062d1d1ae1549592370a572af9a3505a1
DrRhea <czajr13@gmail.com>
2024-06-22 17:26:21 +0700

    add 2 demo projects

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,30 +1,48 @@
 import Work1 from "../../assets/work1.png"
 import Work2 from "../../assets/work2.png"
 import Work3 from "../../assets/work3.png"
+import Work4 from "../../assets/work4.png"
+import Work5 from "../../assets/work5.png"
 
 export const projectsData = [
   {
     id: 1,
     image: Work1,
+    title: "Portfolio",
+    category: "web",
+    link: "https://jagad.vercel.app/",
+    button: "See Project"
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "JPM Rent",
+    category: "web",
+    link: "https://jpmrent.com/",
+    button: "See Project"
+  },
+  {
+    id: 3,
+    image: Work3,
     title: "Feedback App",
     category: "web",
     link: "https://github.com/DrRhea/feedback-app/",
     button: "Source Code"
   },
   {
-    id: 2,
-    image: Work2,
-    title: "Portfolio Website",
-    category: "web",
-    link: "https://jagad.vercel.app/",
-    button: "See Project"
-  },
-  {
-    id: 3,
-    image: Work3,
-    title: "KitSakit Website",
+    id: 4,
+    image: Work4,
+    title: "KitSakit",
     category: "web",
     link: "https://github.com/DrRhea/kitsakit/",
+    button: "Source Code"
+  },
+  {
+    id: 5,
+    image: Work5,
+    title: "r h e n t",
+    category: "web",
+    link: "https://github.com/DrRhea/TubesDPW/",
     button: "Source Code"
   },
 ];
