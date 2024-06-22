f768ce47137ed1040ee8588de14f6da4bc3f7f17
DrRhea <czajr13@gmail.com>
2024-06-22 17:37:31 +0700

    add 3 demo projects

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -3,6 +3,7 @@ import Work2 from "../../assets/work2.png"
 import Work3 from "../../assets/work3.png"
 import Work4 from "../../assets/work4.png"
 import Work5 from "../../assets/work5.png"
+import Work6 from "../../assets/work6.png"
 
 export const projectsData = [
   {
@@ -24,22 +25,30 @@ export const projectsData = [
   {
     id: 3,
     image: Work3,
+    title: "Shark Soar",
+    category: "web",
+    link: "https://rhe-games.web.app/",
+    button: "See Project"
+  },
+  {
+    id: 4,
+    image: Work4,
     title: "Feedback App",
     category: "web",
     link: "https://github.com/DrRhea/feedback-app/",
     button: "Source Code"
   },
   {
-    id: 4,
-    image: Work4,
+    id: 5,
+    image: Work5,
     title: "KitSakit",
     category: "web",
     link: "https://github.com/DrRhea/kitsakit/",
     button: "Source Code"
   },
   {
-    id: 5,
-    image: Work5,
+    id: 6,
+    image: Work6,
     title: "r h e n t",
     category: "web",
     link: "https://github.com/DrRhea/TubesDPW/",
